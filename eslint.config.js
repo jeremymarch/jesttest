d8c989de5f4e6e37118cd8ad7c640df56ba4dc4c
jeremymarch <jmarch@gradcenter.cuny.edu>
2024-11-14 01:07:36 -0500

    update to check *.config.js too

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,15 @@
+/* global require module */
 const eslint = require("@eslint/js");
 
 module.exports = [
-  {
-    ignores: ["**/*.config.js",]
-  },
-  {
-  files: ["**/*.js"]
-  },
-  eslint.configs.recommended,
-  {
-      rules: {
-          semi: "error",
-          "indent": ["error", 4],
-      }
-  }
+    {
+        files: ["**/*.js"]
+    },
+    eslint.configs.recommended,
+    {
+        rules: {
+            semi: "error",
+            "indent": ["error", 4],
+        }
+    }
 ];
